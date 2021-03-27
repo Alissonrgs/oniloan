e4d48cb723d53834e068b684220c10183bf3ba2a
alissonrgs <alissonrgs1993@gmail.com>
2021-03-27 15:49:22 -0300

    Update local settings

--- oniloan/local_settings.example.py
+++ oniloan/local_settings.example.py
@@ -1,3 +1,10 @@
+# https://docs.djangoproject.com/en/3.1/ref/settings/#installed-apps
 LOCAL_INSTALLED_APPS = [
-    'drf_yasg'
+    'drf_yasg',
+    # 'silk'
+]
+
+# https://docs.djangoproject.com/en/3.1/ref/settings/#middleware
+LOCAL_MIDDLEWARE = [
+    # 'silk.middleware.SilkyMiddleware'
 ]
